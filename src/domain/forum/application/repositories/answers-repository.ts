a9b89eee6592b4e659b402173688b97dd64dea0b
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-23 19:26:14 -0300

    feat: implements delete answer

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -2,6 +2,10 @@ import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
   
+  findById(id: string): Promise<Answer | null>
+
   create(answer: Answer): Promise<void>
+
+  delete(answer: Answer): Promise<void>
 
 }
