150bf4bbb6b7439a5963dcfec1326012de74b3fd
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-23 15:32:44 -0300

    feat: implements delete question

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,9 +1,13 @@
-import { Question } from "../../enterprise/entities/question";
+import { Question } from '../../enterprise/entities/question'
 
 export interface QuestionRepository {
+
+  findById(id: string): Promise<Question | null>
 
   findBySlug(slug: string): Promise<Question | null> 
 
   create(question: Question): Promise<void>
+
+  delete(question: Question): Promise<void>
 
 }
