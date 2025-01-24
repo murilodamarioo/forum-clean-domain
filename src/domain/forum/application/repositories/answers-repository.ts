dccdcf3535eed4e21398ac7c4906cd861c266fd5
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-24 14:09:37 -0300

    feat: implements fetch question answers use case and tests

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,8 +1,11 @@
+import { PaginationParams } from './../../../../core/repositories/pagination-params';
 import { Answer } from '../../enterprise/entities/answer' 
 
 export interface AnswersRepository {
   
   findById(id: string): Promise<Answer | null>
+
+  findManyByQuestionId(questionId: string, params: PaginationParams): Promise<Answer[]>
 
   create(answer: Answer): Promise<void>
 
