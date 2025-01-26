81ea941c04001c43ba7b03aea2d8b570f0cb8f24
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-26 00:34:38 -0300

    refactor: applies error handling to get question by slug use case

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,13 +1,14 @@
-import { Either, right } from '@/core/either'
+import { Either, left, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
-type GetQuestionBySlugUseCaseResponse = Either<null, { question: Question }>
+type GetQuestionBySlugUseCaseResponse = Either<ResourceNotFoundError, { question: Question }>
 
 export class GetQuestionBySlugUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
@@ -16,7 +17,7 @@ export class GetQuestionBySlugUseCase {
     const question  = await this.questionsRepository.findBySlug(slug)
 
     if (!question) {
-      throw new Error('Question not found')
+      return left(new ResourceNotFoundError)
     }
 
     return right({ question })
