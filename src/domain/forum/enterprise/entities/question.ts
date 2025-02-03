49baae3f425ad0c3a165c1e1047442a97a297285
murilodamarioo <murilo.gcdamario@gmail.com>
2025-02-02 23:22:54 -0300

    refactor: Editing question with attachments

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,8 +3,7 @@ import { Slug } from './value-objects/slug'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
-import { QuestionAttachment } from './question-attachment'
-import { Attchament } from './attachment'
+import { QuestionAttachmentList } from './question-attachment-list'
 
 export interface QuestionProps {
   authorId: UniqueEntityID
@@ -12,7 +11,7 @@ export interface QuestionProps {
   title: string
   content: string
   slug: Slug
-  attachments: QuestionAttachment[]
+  attachments: QuestionAttachmentList
   createdAt: Date
   updatedAt?: Date
 }
@@ -43,7 +42,7 @@ export class Question extends AggregateRoot<QuestionProps> {
     return this.props.attachments
   }
 
-  set attachments(attachments: QuestionAttachment[]) {
+  set attachments(attachments: QuestionAttachmentList) {
     this.props.attachments = attachments
   }
 
@@ -87,7 +86,7 @@ export class Question extends AggregateRoot<QuestionProps> {
     const question = new Question({
       ...props,
       slug: props.slug ?? Slug.createFromText(props.title),
-      attachments: props.attachments ?? [],
+      attachments: props.attachments ?? new QuestionAttachmentList(),
       createdAt: props.createdAt ?? new Date()
     }, id)
 
