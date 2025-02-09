4c20f3cf1fadbbfe77e5bc10a8fe3417c176801a
murilodamarioo <murilo.gcdamario@gmail.com>
2025-02-09 18:01:06 -0300

    feat: listening event - answer created

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -2,6 +2,8 @@ import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { AnswerAttachmentList } from './answer-attachment-list'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
+import { AnswerCretedEvent } from './events/answer-created-event'
 
 export interface AnswerProps {
   authorId: UniqueEntityID
@@ -12,7 +14,7 @@ export interface AnswerProps {
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   
   get authorId() {
     return this.props.authorId
@@ -62,6 +64,12 @@ export class Answer extends Entity<AnswerProps> {
       attachments: props.attachments ?? new AnswerAttachmentList(),
       createdAt: props.createdAt ?? new Date()
     }, id)
+
+    const isNewAnswer = !id
+
+    if (isNewAnswer) {
+      answer.addDomainEvent(new AnswerCretedEvent(answer))
+    }
 
     return answer
   }
